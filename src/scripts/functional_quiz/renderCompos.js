f6d4491c154f290665221c5eb0c5e5b108706666
ImranGreet <88135011+ImranGreet@users.noreply.github.com>
2024-02-28 14:34:00 +0600

    back button is also defined

--- src/scripts/functional_quiz/renderCompos.js
+++ src/scripts/functional_quiz/renderCompos.js
@@ -6,12 +6,14 @@ let rendredCompo = ref(0);
 const validationError = ref([]);
 
 const compoentToBeRender = function (compoNumber) {
-    rendredCompo.value =compoNumber
-   
+    rendredCompo.value =compoNumber;
+    browsedCompos.value.push(compoNumber);
+    browsedCompos.value = [...new Set(browsedCompos.value)];
     return rendredCompo;
 }
 const gotoPreviusPage = function(){
-    rendredCompo.value --;
+    const moveToComponent = browsedCompos.value.pop();
+    rendredCompo.value = moveToComponent
     return rendredCompo;
 }
 export {rendredCompo,validationError,compoentToBeRender,gotoPreviusPage}
