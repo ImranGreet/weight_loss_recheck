39d231392d351a90e4c4fdd8eeddaf21a1abfac3
ImranGreet <88135011+ImranGreet@users.noreply.github.com>
2024-03-07 00:07:52 +0600

    navbar need to add

--- src/Layouts/Client.vue
+++ src/Layouts/Client.vue
@@ -2,20 +2,39 @@
     <section
       class="w-full flex flex-col justify-center items-center min-h-screen"
     >
+    <nav class="w-full flex justify-between items-center bg-primary p-5">
+      <router-link to="#">evony</router-link>
+      <ul class="flex space-x-3">
+        <li>
+          <router-link to="#">
+            <UserIcon class="w-5 h-5 inline-block"/>
+          </router-link>
+        </li>
+        <li>
+          <router-link to="#">
+            <ShoppingCartIcon class="w-5 h-5 inline-block"/>
+          </router-link>
+        </li>
+      </ul>
+    </nav>
       <router-view />
       <Footer />
     </section>
   </template>
   
   <script>
+import { ShoppingCartIcon, UserIcon } from '@heroicons/vue/20/solid';
+
  
   
   import Footer from "../components/utility/Footer.vue";
   
+  
   export default {
     name: "Client",
     components: {
-    
+      UserIcon,
+      ShoppingCartIcon,
       Footer,
     },
   };
