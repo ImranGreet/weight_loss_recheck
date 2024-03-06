7eef8c61f6d755c4110ee8fcf0f8a6a572cfcf9a
ImranGreet <88135011+ImranGreet@users.noreply.github.com>
2024-03-07 00:13:57 +0600

    navbar need to add

--- src/Layouts/Client.vue
+++ src/Layouts/Client.vue
@@ -3,15 +3,15 @@
       class="w-full flex flex-col justify-center items-center min-h-screen"
     >
     <nav class="w-full flex justify-between items-center bg-primary p-5">
-      <router-link to="#">evony</router-link>
+      <router-link to="#" class="text-xl xl:text-6xl font-black">evony</router-link>
       <ul class="flex space-x-3">
         <li>
-          <router-link to="#">
+          <router-link to="#" class="bg-gray-400/60 p-1 flex justify-center items-center rounded-full">
             <UserIcon class="w-5 h-5 inline-block"/>
           </router-link>
         </li>
         <li>
-          <router-link to="#">
+          <router-link to="# " >
             <ShoppingCartIcon class="w-5 h-5 inline-block"/>
           </router-link>
         </li>
